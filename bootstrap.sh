dfb1efe222f49665e9ed002cdf49e5d20de2da31
Wayne Huang <wayne@gadgetism.com>
2020-09-07 00:13:41 -0700

    adding ssh config, bootstrap and bash prompt

---
+++ bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+
+function doIt() {
+  rsync \
+    --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude "bootstrap.sh" \
+    --exclude "*.un~" \
+    --exclude "README.md" \
+    --exclude ".jshintrc" \
+    --exclude "LICENSE" -av --no-perms . ~
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doIt
+  reset
+elif [ "$1" == "--provision" -o "$1" == "-p" ]; then
+  doIt
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+    reset
+    chmod 644 $HOME/.ssh/config*
+  fi
+fi
+unset doIt
