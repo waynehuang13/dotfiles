4e4afbbe8de64323dec1f3dc2b5bdcd116ba2d2d
Wayne Huang <waynehuang13@gmail.com>
2025-01-03 11:41:59 -0800

    fixed aliases and added ssh config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,11 +10,12 @@ alias ap="ansible-playbook"
 # alias code="open -a 'Visual Studio Code'"
 alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 
-alias gwp="cd $HOME/workspaces"
-alias gcs="cd $HOME/workspaces/cheat-sheets"
-alias gbp="cd $HOME/workspaces/boilerplates"
+alias gwp="cd $HOME/workspace"
+alias gcs="cd $HOME/workspace/cheat-sheets"
+alias gbp="cd $HOME/workspace/boilerplates"
 
 alias ls="eza --icons --group-directories-first"
+alias l="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -lha"
 alias grep='grep --color'
 # alias grep='nocorrect grep --color=auto'
