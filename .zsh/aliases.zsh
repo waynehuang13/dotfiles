20ff69fbc727d5831212ab73b6370e0c607b5df7
Wayne Huang <waynehuang13@gmail.com>
2025-01-02 22:37:35 -0800

    more aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,7 +7,8 @@ alias tf="terraform"
 alias a="ansible"
 alias ap="ansible-playbook"
 
-alias code="open -a 'Visual Studio Code'"
+# alias code="open -a 'Visual Studio Code'"
+alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 
 alias gwp="cd $HOME/workspaces"
 alias gcs="cd $HOME/workspaces/cheat-sheets"
@@ -15,11 +16,12 @@ alias gbp="cd $HOME/workspaces/boilerplates"
 
 alias ls="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -lha"
-#alias grep='grep --color'
-
-
+alias grep='grep --color'
+# alias grep='nocorrect grep --color=auto'
+alias cat="bat --paging=never"
 alias vi=nvim
 alias vim=nvim
 
-alias grep='nocorrect grep --color=auto'
 alias vm_restartdhcpd='sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop;sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start'
+
+
